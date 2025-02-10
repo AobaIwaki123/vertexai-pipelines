6be834379735a0e236959e8a511a8e15a4d9a87b
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2025-02-10 19:36:54 +0900

    fix code

--- examples/pipelines/providers/google_vertexai_manifold_pipeline.py
+++ examples/pipelines/providers/google_vertexai_manifold_pipeline.py
@@ -130,7 +130,7 @@ class Pipeline:
                 (msg["content"] for msg in messages if msg["role"] == "system"), None
             )
 
-            print(f"System message: {system_message}")
+            # print(f"System message: {system_message}")
 
             model = GenerativeModel(
                 model_name=model_id,
