a9a256be05540ab75a519673ec83d809934fa854
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2025-02-10 18:24:34 +0900

    langchain comment out

--- examples/pipelines/providers/google_vertexai_manifold_pipeline.py
+++ examples/pipelines/providers/google_vertexai_manifold_pipeline.py
@@ -57,18 +57,18 @@ class Pipeline:
             {"id": "gemini-1.5-pro-001", "name": "Gemini 1.5 Pro"},
         ]
 
-        embedding = VertexAIEmbeddings(
-            model_name="textembedding-gecko-multilingual@latest",
-            project=self.valves.GOOGLE_PROJECT_ID,
-        )
-
-        self.store = BigQueryVectorSearch(
-            project_id=self.valves.GOOGLE_PROJECT_ID,
-            dataset_name="law_db",
-            table_name="personal_info",
-            embedding=embedding,
-            distance_strategy=DistanceStrategy.COSINE,
-        )
+#         embedding = VertexAIEmbeddings(
+#             model_name="textembedding-gecko-multilingual@latest",
+#             project=self.valves.GOOGLE_PROJECT_ID,
+#         )
+# 
+#         self.store = BigQueryVectorSearch(
+#             project_id=self.valves.GOOGLE_PROJECT_ID,
+#             dataset_name="law_db",
+#             table_name="personal_info",
+#             embedding=embedding,
+#             distance_strategy=DistanceStrategy.COSINE,
+#         )
 
     async def on_startup(self) -> None:
         """This function is called when the server is started."""
