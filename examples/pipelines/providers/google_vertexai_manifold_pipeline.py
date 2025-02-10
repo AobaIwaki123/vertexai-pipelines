b909fda62f305a25c97cde7379b4ab9c5708e4cf
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2025-02-10 17:48:20 +0900

    downgrade pydantic

--- examples/pipelines/providers/google_vertexai_manifold_pipeline.py
+++ examples/pipelines/providers/google_vertexai_manifold_pipeline.py
@@ -5,7 +5,7 @@ date: 2024-09-19
 version: 1.0
 license: MIT
 description: A pipeline for generating text using Google's GenAI models in Open-WebUI.
-requirements: vertexai, langchain, langchain-community, langchain-google-vertexai==2.0.7
+requirements: vertexai, langchain, langchain-community, langchain-google-vertexai==2.0.7, pydantic==2.10.5
 environment_variables: GOOGLE_PROJECT_ID, GOOGLE_CLOUD_REGION, BIG_QUERY_DATASET, BIG_QUERY_TABLE
 usage_instructions:
   To use Gemini with the Vertex AI API, a service account with the appropriate role (e.g., `roles/aiplatform.user`) is required.
